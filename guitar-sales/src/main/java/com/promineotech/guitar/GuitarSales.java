17572d9bfb7d599aee59a2b87a82778ab99796de
Dylan Crowell <dcrowell37@gmail.com>
2021-11-22 18:35:00 -0700

    first commit

---
+++ guitar-sales/src/main/java/com/promineotech/guitar/GuitarSales.java
@@ -0,0 +1,13 @@
+package com.promineotech.guitar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuitarSales {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GuitarSales.class, args);
+  }
+
+}
